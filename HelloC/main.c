fd80b4670652593c904dd1a2c8bd301256d6aa63
hanquanwei2010 <hanquanwei2012@gmail.com>
2012-10-10 11:46:54 +0800

    Initial Commit

---
+++ HelloC/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HelloC
+//
+//  Created by hanqw on 12-10-10.
+//  Copyright (c) 2012年 hanqw. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
