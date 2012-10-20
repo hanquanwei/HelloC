dff7b8d2149bb9ffdae29f6628746998d376718f
hanquanwei2010 <hanquanwei2012@gmail.com>
2012-10-20 16:34:39 +0800

    学习c
    学习

--- HelloC/main.c
+++ HelloC/main.c
@@ -7,12 +7,42 @@
 //
 
 #include <stdio.h>
+#include <ctype.h>
+#define INT_LENGTH 32
+
+unsigned int reverse_int(unsigned int value);
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    printf("Hello, World!\n");
+    unsigned answer=25;
+    unsigned result=0;
+    int c=0;
+    int len=0;
+    while (c=answer&0x1,answer>0&&len<=INT_LENGTH) {
+        len++;
+        result<<=1;
+        result+=c<<1;
+        answer>>=1;
+    }
+    while (len<INT_LENGTH-1) {
+        result<<=1;
+        ++len;
+    }
+    printf("result is %ul",reverse_int(25));
     return 0;
 }
 
+unsigned int reverse_int(unsigned int value){
+    unsigned result=0;
+    unsigned int i;
+    for (i=1; i!=0; i<<=1) {
+        result<<=1;
+        if(value&1){
+            result|=1;
+        }
+        value>>=1;
+    }
+    return result;
+}
